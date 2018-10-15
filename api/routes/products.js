dc54f49f5fc3b5121bc318073537e3f09b05efc8
lmanzanero <manzanero.luis1995@gmail.com>
2018-10-14 18:38:54 -0600

    Added products and orders routes'

---
+++ api/routes/products.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) =>  {
+    res.status(200).json({
+        message: 'handing get requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) =>  {
+    res.status(200).json({
+        message: 'handing post requests to /products'
+    });
+});
+
+router.post('/:productId', (req, res, next) =>  {
+   const id = req.params.productId;
+   if(id === 'special'){
+       res.status(200).json({
+        message: 'You discovered the special ID'
+       });
+   } else {
+       res.status(200).json({
+        message: 'you pass an ID'
+       });
+   }
+});
+
+router.patch('/:productId', (req, res, next) =>  {
+     res.status(200).json({
+         message: 'Updated Product'
+    });
+ });
+
+ router.delete('/:productId', (req, res, next) =>  {
+  
+     res.status(200).json({
+         message: 'Deleted Product!'
+    });
+
+ });
+
+module.exports = router;
